791338b1277583500d692f948dc712c8725d356f
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-06-14 17:14:56 +0200

    aggiunta funzione dichiara unità di misura con rispettivo validator

--- src/jana60/geometria/Main.java
+++ src/jana60/geometria/Main.java
@@ -27,31 +27,50 @@ public class Main {
 
 		int[] bRettangolo = new int[5];
 		int[] hRettangolo = new int[5];
+		boolean valid = false; // Validator dei valori digitati dall'utente
 
-		boolean valid = false;
+		String unit; // Unità di misura
+		String validUnits[] = { "km", "hm", "dam", "m", "dm", "cm", "mm" }; // Unità accettate
+
+		do { // Se l'utente inserisce unità non accettate, ripeti il ciclo
+				// Chiedo all'utente di specificare un'unità di misura
+			System.out.println("Dichiara un'unità di misura:");
+			unit = scan.nextLine();
+			for (int i = 0; i < validUnits.length; i++) {
+				if (unit.equalsIgnoreCase(validUnits[i])) {
+					valid = true;
+				}
+			}
+			if (valid == false) {
+				System.out.println("Hai inserito un'unità non presente nel sistema. Prova con 'cm'.");
+			}
+		} while (valid == false);
+
 		for (int i = 0; i < 5; i++) {
 			// Se il valore inserito è minore di zero, ripeti il ciclo
 
 			do {// Se inserisco valori negativi, il ciclo si ripete per l'iterazione in corso
 				// Chiedo i valori all'utente e li raccolgo nell'array
-				System.out.println("Quanto misura la base del rettangolo numero " + (i + 1) + "?");
+				System.out.print("Quanto misura la base del rettangolo numero " + (i + 1) + " in " + unit + "? ");
 				bRettangolo[i] = scan.nextInt();
-				System.out.println("Quanto misura l'altezza del rettangolo numero " + (i + 1) + "?");
+				System.out.print("Quanto misura l'altezza del rettangolo numero " + (i + 1) + " in " + unit + "? ");
 				hRettangolo[i] = scan.nextInt();
+
+				// Controllo che i valori siano entrambi maggiori di zero
 				if (bRettangolo[i] > 0 && hRettangolo[i] > 0) {
 					valid = true;
 				} else {
 					valid = false;
-					System.out.println("\nERRORE: " + "Non puoi inserire numeri negativi!\n");
+					System.out.println("\nERRORE: Hai inserito valori non validi!\n");
 				}
 			} while (valid == false);
 		}
 
 		// Recap dei valori scelti dall'utente
 		System.out.println("\nRiepilogando:");
-		for (int i = 0; i < 5; i++) {
-			System.out.println("Il rettangolo numero " + (i + 1) + " ha i seguenti valori: ");
-			System.out.println("b = " + bRettangolo[i] + "; " + "h = " + hRettangolo[i] + ";");
+		for (int i = 0; i < bRettangolo.length && i < hRettangolo.length; i++) {
+			System.out.println("Rettangolo numero " + (i + 1) + ": ");
+			System.out.println("b = " + bRettangolo[i] + unit + "; " + "h = " + hRettangolo[i] + unit + ";");
 		}
 
 		// Calcolo l'Area dei rettangoli
@@ -69,7 +88,7 @@ public class Main {
 		}
 
 		// Stampo l'Area del rettangolo più grande
-		System.out.println("\nL'area maggiore è uguale a: " + aMaggiore);
+		System.out.println("\nL'area maggiore è uguale a: " + aMaggiore + unit + "\u00B2");
 		scan.close();
 	}
 
