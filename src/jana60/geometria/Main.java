ae0bf4a45a1c728d5a1dd61908c8c091c5ea5407
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-06-14 15:28:24 +0200

    iniziato bonus, aggiunto array per valori rettangolo

--- src/jana60/geometria/Main.java
+++ src/jana60/geometria/Main.java
@@ -9,17 +9,37 @@ public class Main {
 		Scanner scan = new Scanner(System.in);
 
 		// Istanza
-		Rettangolo rettangolo1 = new Rettangolo();
+		Rettangolo mioRettangolo = new Rettangolo();
 
-		// Chiedo i valori all'utente
-		System.out.print("Inserisci la base del rettangolo: ");
-		rettangolo1.base = scan.nextInt();
-		System.out.print("Inserisci l'altezza del rettangolo: ");
-		rettangolo1.altezza = scan.nextInt();
+// ___VERSIONE BASE___
+//		// Chiedo i valori all'utente
+//		System.out.print("Inserisci la base del rettangolo: ");
+//		mioRettangolo.base = scan.nextInt();
+//		System.out.print("Inserisci l'altezza del rettangolo: ");
+//		mioRettangolo.altezza = scan.nextInt();
+//
+//		// Stampo Perimetro
+//		System.out.println("Ecco i risultati:\n");
+//		mioRettangolo.stampaRisultati();
+//______________________________________________________________________//
 
-		// Stampo Perimetro
-		System.out.println("Ecco i risultati:\n");
-		rettangolo1.stampaRisultati();
+//___VERSIONE BONUS___
+
+		int[] bRettangolo = new int[5];
+		int[] hRettangolo = new int[5];
+
+		for (int i = 0; i < 5; i++) {
+			// Chiedo i valori all'utente e li raccolgo nell'array
+			System.out.println("Quanto misura la base del rettangolo numero " + (i + 1) + "?");
+			bRettangolo[i] = scan.nextInt();
+			System.out.println("Quanto misura l'altezza del rettangolo numero " + (i + 1) + "?");
+			hRettangolo[i] = scan.nextInt();
+		}
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("\nIl rettangolo numero " + (i + 1) + " ha i seguenti valori: \n" + "b = "
+					+ bRettangolo[i] + "; " + "h = " + hRettangolo[i] + ";");
+		}
 
 		scan.close();
 	}
